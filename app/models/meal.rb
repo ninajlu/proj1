21e8bf8bdbe94621688ab1fc882d0027c03bb47a
Nina Lu <ninajlu@gmail.com>
2013-03-30 09:03:42 -0400

    added dropdowns

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ActiveRecord::Base
-  attr_accessible :end, :start, :type
+  attr_accessible :end, :start, :type, :venue_id
   belongs_to :venue
+  def meal_venue
+  	meal.venue if venue
+  end
 end
